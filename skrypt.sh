08c361b16f97de608dac7bf26540b39f5d46c102
mashashelegeda <masha0ahelegeda@gmail.com>
2024-04-17 20:19:12 +0300

    pliki log

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,21 @@
-#!bin/bash/
-date=$(date +"%Y_%m-%d")
-echo "Current date: $date"
+#!bin/bash
+if [ "$1" = "--date" ]; then
+    date=$(date +"%Y_%m-%d")
+    echo "Current date: $date"
+fi
 
+if [ "$1" = "--logs" ]; then
+    if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+        count=$2
+    else
+        count=100
+    fi
+
+    for ((i=1; i<=$count; i++))
+    do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > $filename
+        echo "Nazwa skryptu: $0" >> $filename
+        echo "Data utworzenia: $(date)" >> $filename
+    done
+fi
