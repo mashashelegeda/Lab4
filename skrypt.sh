8bd0ea8921aab810719a1cf9a83889c5785af5b4
mashashelegeda <masha0ahelegeda@gmail.com>
2024-04-20 16:37:43 +0300

    --error added

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,10 @@
 #!bin/bash
-if [ "$1" = "--date" ]; then
+if [ "$1" = "--date" ] || [ "$1" = "-d" ]; then
     date=$(date +"%Y_%m-%d")
     echo "Current date: $date"
 fi
 
-if [ "$1" = "--logs" ]; then
+if [ "$1" = "--logs" ] || [ "$1" = "-l" ]; then
     if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
         count=$2
     else
@@ -20,5 +20,25 @@ if [ "$1" = "--logs" ]; then
     done
 fi
 
-if [ "$1" = "--help" ]; then
-    echo "--date  --logs --logs 30"
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    echo "--date -d  --logs -l --logs 30 --help -h  --init -error"
+fi
+if [ "$1" = "--init" ]; then
+    git clone https://github.com/mashashelegeda/Lab4
+    export PATH=$PATH:/C:/Mariia_univ/lab4/Lab4
+    exit 0
+fi
+if [ "$1" = "--error" ] || [ "$1" = "-e" ]; then
+    if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+        count=$2
+    else
+        count=100
+    fi
+
+    for ((i=1; i<=$count; i++))
+    do
+        error_file="error$i.txt"
+        echo "Error message" > "error_files/$error_file"
+    done
+    exit 0
+fi
